9569581dfcf9be7a0637887228164c1c2b5f7ec9
Basil Wabuke <babazwabuke4@gmail.com>
2024-10-25 14:13:37 +0300

    adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Your code goes here
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("The DOM has loaded");
+});
+console.log("This console.log fires when the index.js loads before DOMContentLoaded is triggered");
+document.addEventListener('DOMContentLoaded', () => {
+    const textElement = document.getElementById('text');
+    textElement.textContent = "This is really cool!";
+});
